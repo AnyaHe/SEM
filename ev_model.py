b345754493d95c34b74a5f31fa6dcf5726077e48
AnyaHe <anya.heider@rl-institut.de>
2022-08-11 13:42:50 +0200

    add loading of flex band per use case

--- ev_model.py
+++ ev_model.py
@@ -118,6 +118,29 @@ def import_flexibility_bands(dir):
     return flexibility_bands
 
 
+def import_flexibility_bands_use_case(dir, use_cases):
+    flexibility_bands = {}
+
+    for band in ["upper_power", "upper_energy", "lower_energy"]:
+        band_df = pd.DataFrame()
+        for use_case in use_cases:
+            flexibility_bands_tmp = \
+                pd.read_csv(dir+'/{}_{}.csv'.format(band, use_case),
+                            index_col=0, parse_dates=True, dtype=np.float32)
+            band_df = pd.concat([band_df, flexibility_bands_tmp],
+                                axis=1)
+        if band_df.columns.duplicated().any():
+            raise ValueError("Charging points with the same name in flexibility bands. "
+                             "Please check")
+        flexibility_bands[band] = band_df
+        # remove numeric problems
+        if "upper" in band:
+            flexibility_bands[band] = flexibility_bands[band] + 1e-6
+        elif "lower" in band:
+            flexibility_bands[band] = flexibility_bands[band] - 1e-6
+    return flexibility_bands
+
+
 def reduced_operation(model):
     return sum(model.charging_ev[time]**2 for time in model.time_set)
 
