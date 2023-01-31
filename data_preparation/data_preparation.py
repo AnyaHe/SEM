3d26359e120e20643f4dd213fedddd1402b99c12
AnyaHe <anya.heider@rl-institut.de>
2023-01-31 15:40:37 +0100

    add method for heat pump data

--- data_preparation/data_preparation.py
+++ data_preparation/data_preparation.py
@@ -1,7 +1,81 @@
 import pandas as pd
 import numpy as np
+import os
 
 BANDS = ["upper_power", "upper_energy", "lower_energy"]
+
+
+def get_heat_pump_timeseries_data(data_dir, scenario_dict):
+    """
+    Method to calculate a germany-wide COP time series by building the weighted sum of
+    input time-series from when2heat
+
+    (https://data.open-power-system-data.org/when2heat/)
+
+    :param data_dir: str
+        directory where when2heat data is stored
+    :param scenario_dict: dict
+        needs entries for cop, see use :func:`merge_cop_heat_pumps` for required keys
+    :return: scaled_cop
+    """
+    hp_path = os.path.join(data_dir, "when2heat.csv")
+    profiles_hp = pd.read_csv(hp_path, sep=';', decimal=",", index_col=0,
+                              parse_dates=True)
+    timesteps = scenario_dict["ts_timesteps"]
+    profiles_hp = profiles_hp.loc[timesteps.tz_localize("UTC")]
+    # calculate heat_demand
+    heat_demand = merge_heat_demand(profiles_hp, scenario_dict)
+    # calculate cop
+    cop = merge_cop_heat_pumps(profiles_hp, scenario_dict)
+    return heat_demand, cop
+
+
+def merge_cop_heat_pumps(profiles_hp, scenario_dict):
+    """
+    Method to calculate a germany-wide COP time series by building the weighted sum of
+    input time-series from when2heat
+
+    (https://data.open-power-system-data.org/when2heat/)
+    :param profiles_hp: pd.DataFrame
+        input time-series from when2heat
+        (https://data.open-power-system-data.org/when2heat/)
+    :param scenario_dict: dict
+        has to have entries "hp_weight_air", "hp_weight_ground", "hp_weight_floor" and
+        "hp_weight_radiator" and "ts_timesteps". Note that hp_weight_air + hp_weight_ground =
+        hp_weight_floor + hp_weight_radiator = 1 has to be given.
+    :return: pd.Series
+        COP time series
+    """
+    source_names = {"air": "ASHP", "ground": "GSHP"}
+    scaled_cop = \
+        sum([scenario_dict[f"hp_weight_{source}"] * scenario_dict[f"hp_weight_{sink}"]
+             * profiles_hp[f'DE_COP_{source_names[source]}_{sink}']
+             for source in ["air", "ground"] for sink in ["floor", "radiator"]])
+    return scaled_cop
+
+
+def merge_heat_demand(profiles_hp, scenario_dict):
+    """
+    Method to calculate Germany wide heat demand by weighting the input time series from
+    when2heat with the share of SFH and MFH (from statista)
+
+    :param profiles_hp: pd.DataFrame
+        input time-series from when2heat
+        (https://data.open-power-system-data.org/when2heat/)
+    :param scenario_dict: dict
+        has to have entries "hp_weight_air", "hp_weight_ground", "hp_weight_floor" and
+        "hp_weight_radiator" and "ts_timesteps". Note that hp_weight_air + hp_weight_ground =
+        hp_weight_floor + hp_weight_radiator = 1 has to be given.
+    :return: pd.Series
+        heat demand time series
+    """
+    scenario_dict["buildings_nr_MFH"] = \
+        scenario_dict["buildings_nr_total"] - scenario_dict["buildings_nr_SFH"]
+    scaled_heat_demand = sum([
+        scenario_dict[f"buildings_nr_{building}"]/scenario_dict[f"buildings_nr_total"] *
+        profiles_hp[f"DE_heat_demand_space_{building}"]for building in ["SFH", "MFH"]
+    ])
+    return scaled_heat_demand
 
 
 def import_flexibility_bands(dir, use_cases):
@@ -25,3 +99,18 @@ def import_flexibility_bands(dir, use_cases):
         elif "lower" in band:
             flexibility_bands[band] = flexibility_bands[band] - 1e-5
     return flexibility_bands
+
+
+if __name__ == "__main__":
+    scenario_dict = {
+        "buildings_nr_SFH": 16.105, # Mio. [Source: Statista]
+        "buildings_nr_total": 19.376,  # Mio. [Source: Statista]
+        "hp_weight_air": 0.71,
+        "hp_weight_ground": 0.29,
+        "hp_weight_floor": 0.6,
+        "hp_weight_radiator": 0.4,
+        "ts_timesteps": pd.date_range("1/1/2011 00:00", periods=8760, freq="H")
+    }
+    hp_dir = r"C:\Users\aheider\Documents\Software\Cost-functions\distribution-grid-expansion-cost-functions\data"
+    scaled_heat_demand, scaled_cop = get_heat_pump_timeseries_data(hp_dir, scenario_dict)
+    print("Success")
