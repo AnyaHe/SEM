47e649b6c3aebc82c3e4ec3fbcd8e8b924ba9644
AnyaHe <anya.heider@rl-institut.de>
2023-09-30 02:39:29 +0200

    add scenario storage implementation

---
+++ sensitivity_analysis_losses_formulation.py
@@ -0,0 +1,158 @@
+import pandas as pd
+import pyomo.environ as pm
+import os
+
+from storage_equivalent.storage_equivalent_model import add_storage_equivalent_model, \
+    minimize_discharging, set_up_base_model
+from scenario_input import base_scenario, scenario_input_hps, save_scenario_dict, \
+    scenario_input_evs, adjust_timeseries_data, get_new_residual_load, \
+    scenario_variation_electric_vehicles_and_heat_pumps
+from storage_equivalent.heat_pump_model import add_heat_pump_model, model_input_hps
+from storage_equivalent.ev_model import add_evs_model, model_input_evs
+from plotting import plot_storage_equivalent_germany_stacked
+
+if __name__ == "__main__":
+    scenario = "sensitivity_implementation_losses"
+    plot_results = False
+    solver = "gurobi"
+    varied_parameter = "losses_implementation"
+    variations = ["linear_penalty", "binaries", "penalty"]
+    # create results directory
+    res_dir = os.path.join(f"results/{scenario}")
+    os.makedirs(res_dir, exist_ok=True)
+    # load scenario values
+    scenario_dict = base_scenario()
+    scenario_input = \
+        scenario_variation_electric_vehicles_and_heat_pumps()["EV_HP_flex+++"]
+    scenario_dict.update(scenario_input)
+    scenario_dict["solver"] = solver
+    scenario_dict["varied_parameter"] = varied_parameter
+    scenario_dict[varied_parameter] = variations
+    if scenario_dict["hp_mode"] is not None:
+        scenario_dict = scenario_input_hps(scenario_dict=scenario_dict,
+                                           mode=scenario_dict["hp_mode"],
+                                           use_binaries=True)
+        if scenario_dict["hp_mode"] == "flexible":
+            scenario_dict["capacity_single_tes"] = \
+                scenario_dict["tes_relative_size"] * scenario_dict[
+                    "capacity_single_tes"]
+        # load ev data if required
+        if scenario_dict["ev_mode"] is not None:
+            scenario_dict = scenario_input_evs(scenario_dict=scenario_dict,
+                                               mode=scenario_dict["ev_mode"],
+                                               use_cases_flexible=scenario_dict[
+                                                   "flexible_ev_use_cases"],
+                                               extended_flex=scenario_dict[
+                                                   "ev_extended_flex"],
+                                               v2g=scenario_dict["ev_v2g"],
+                                               use_binaries=True)
+    # shift timeseries
+    scenario_dict = adjust_timeseries_data(scenario_dict)
+    # adjust model input to hps and evs
+    nr_hp_mio, ts_heat_el, sum_energy_heat, capacity_tes, p_nom_hp, ts_heat_demand = \
+        model_input_hps(
+            scenario_dict=scenario_dict,
+            hp_mode=scenario_dict["hp_mode"],
+            nr_hp_mio=19.4
+        )
+    nr_ev_mio, flexibility_bands, energy_ev, ref_charging = model_input_evs(
+        scenario_dict=scenario_dict,
+        ev_mode=scenario_dict["ev_mode"],
+        nr_ev_mio=48.8
+    )
+    new_res_load = get_new_residual_load(scenario_dict,
+                                         sum_energy_heat=sum_energy_heat,
+                                         energy_ev=energy_ev,
+                                         ref_charging=ref_charging,
+                                         ts_heat_el=ts_heat_el)
+    shifted_energy_df = pd.DataFrame(columns=["relative_weight", "storage_type",
+                                              "energy_stored"])
+    shifted_energy_rel_df = pd.DataFrame(columns=["relative_weight", "storage_type",
+                                                  "energy_stored"])
+    for val in scenario_dict[scenario_dict["varied_parameter"]]:
+        model = set_up_base_model(scenario_dict=scenario_dict,
+                                  new_res_load=new_res_load)
+        use_binaries = val == "binaries"
+        use_linear_penalty = val == "linear_penalty"
+
+        if scenario_dict["hp_mode"] == "flexible":
+            model = add_heat_pump_model(model, p_nom_hp, capacity_tes,
+                                        scenario_dict["ts_cop"], ts_heat_demand,
+                                        efficiency_static_tes=scenario_dict[
+                                            "efficiency_static_tes"],
+                                        efficiency_dynamic_tes=scenario_dict[
+                                            "efficiency_dynamic_tes"],
+                                        use_binaries=use_binaries,
+                                        use_linear_penalty=use_linear_penalty,
+                                        weight_hp=0.2)
+        if scenario_dict["ev_mode"] == "flexible":
+            add_evs_model(model, flexibility_bands,
+                          v2g=scenario_dict["ev_v2g"],
+                          efficiency=scenario_dict["ev_charging_efficiency"],
+                          discharging_efficiency=scenario_dict[
+                              "ev_discharging_efficiency"],
+                          use_binaries=use_binaries,
+                          use_linear_penalty=use_linear_penalty,
+                          weight_ev=0.25)
+        model = add_storage_equivalent_model(model, new_res_load,
+                                             time_horizons=scenario_dict["time_horizons"])
+        model.objective = pm.Objective(rule=minimize_discharging,
+                                       sense=pm.minimize,
+                                       doc='Define objective function')
+        opt = pm.SolverFactory(solver)
+        # if solver == "gurobi":
+        #     opt.options["Method"] = 3
+        if val == "penalty":
+            opt.options["NonConvex"] = 2
+        results = opt.solve(model, tee=True)
+        charging = pd.Series(model.charging.extract_values()).unstack()
+        energy_levels = pd.Series(model.energy_levels.extract_values()).unstack()
+        discharging = pd.Series(model.discharging.extract_values()).unstack()
+        df_tmp = (discharging.sum(axis=1)).reset_index().rename(
+            columns={"index": "storage_type", 0: "energy_stored"})
+        df_tmp[scenario_dict["varied_parameter"]] = val
+        shifted_energy_df = shifted_energy_df.append(df_tmp, ignore_index=True)
+        df_tmp["energy_stored"] = \
+            df_tmp["energy_stored"] / scenario_dict["ts_demand"].sum().sum() * 100
+        shifted_energy_rel_df = shifted_energy_rel_df.append(df_tmp,
+                                                             ignore_index=True)
+        # extract charging and discharging of EV and TES
+        sc_operations = pd.DataFrame()
+        # EV charging and discharging
+        ev_charging = pd.Series(
+            model.charging_ev.extract_values()).unstack().T
+        ev_discharging = pd.Series(
+            model.discharging_ev.extract_values()
+        ).unstack().T
+        if use_binaries:
+            ev_charging *= pd.Series(
+                model.y_charge_ev.extract_values()).unstack().T
+            ev_discharging *= pd.Series(
+                        model.y_discharge_ev.extract_values()
+                    ).unstack().T
+        sc_operations["EV_charging"] = ev_charging.iloc[:, 0]
+        sc_operations["EV_discharging"] = ev_discharging.iloc[:, 0]
+        # HP charging and discharging
+        charging_tes = pd.Series(model.charging_tes.extract_values())
+        discharging_tes = pd.Series(
+            model.discharging_tes.extract_values())
+        if use_binaries:
+            charging_tes *= pd.Series(
+                model.y_charge_tes.extract_values())
+            discharging_tes *= pd.Series(
+                model.y_discharge_tes.extract_values())
+        sc_operations["TES_charging"] = charging_tes
+        sc_operations["TES_discharging"] = discharging_tes
+        sc_operations.index = scenario_dict["ts_demand"].index
+        sc_operations.to_csv(f"{res_dir}/sc_operations_{val}.csv")
+    shifted_energy_df.to_csv(f"{res_dir}/storage_equivalents.csv")
+    shifted_energy_rel_df.to_csv(
+        f"{res_dir}/storage_equivalents_relative.csv")
+    if plot_results:
+        plot_storage_equivalent_germany_stacked(
+            shifted_energy_df,
+            parameter={
+                "relative_weight": "Relative Weight [-]"})
+    # remove timeseries as they cannot be saved in json format
+    save_scenario_dict(scenario_dict, res_dir)
+    print("SUCCESS")
