301b11966c659064409b164870930a1e3a3e55d8
aheider <anya.heider@rl-institut.de>
2024-04-24 16:24:04 +0200

    update scenario for evaluation of tariffs

--- scenario_NTs.py
+++ scenario_NTs.py
@@ -10,10 +10,60 @@ from storage_equivalent import set_up_base_model, add_storage_equivalent_model, 
     minimize_discharging, determine_storage_durations
 
 
+def load_adapted_timeseries_per_grid(gd_dir, gd_id, nt, ts_hh):
+    households = \
+        pd.read_csv(os.path.join(gd_dir, str(gd_id),
+                                 f"households_{penetration}.csv"), index_col=0)
+    household_mapping = pd.read_csv(os.path.join(gd_dir, str(gd_id), "mapping.csv"),
+                                    index_col=0).set_index("name_old")
+    ts_load = pd.DataFrame()
+    for (has_ev, has_hp, has_pv, has_bess), household_group in households.groupby(
+            ["has_ev", "has_hp", "has_pv", "has_bess"]):
+        # get names of consumers in timeseries
+        names_new = household_mapping.loc[household_group.index, "name_new"]
+        # determine group of time series
+        customer_group = [has_ev * "EV", has_hp * "HP", has_pv * "PV",
+                          has_bess * "BESS"]
+        customer_group = "_".join(list(filter(("").__ne__, customer_group)))
+        if customer_group == "":
+            customer_group = "HH"
+        # load time series and results
+        ts_customer_group = pd.read_csv(
+            os.path.join(ts_dir, customer_group, nt, "timeseries.csv"),
+            index_col=0, parse_dates=True)
+        ts_load_tmp = ts_customer_group.loc[:, names_new]
+        ts_load_tmp.columns = household_group.index
+        ts_load = pd.concat([ts_load, ts_load_tmp], axis=1)
+        # save original load
+        if customer_group == "HH":
+            ts_hh += ts_customer_group.loc[:, household_mapping.name_new].sum(axis=1)
+    return ts_load, ts_hh
+
+
 if __name__ == "__main__":
-    scenario = "Variation_pricing"
-    tariffs = ['Cl', 'Csl', 'Ec', 'Ec_Cl', 'Ec_Clf', 'Ec_Clf_Sv', 'Ec_Cl_Sv', 'Ec_Sv',
-               'Edn', 'Edn_Cl', 'Edn_Clf', "Ec_Sr_capped", "Ec_Sr_shifted"]
+    penetration = 0.1
+
+    data_dir = r"H:\Tariffs"
+    grid_dir = os.path.join(data_dir, "Grids")
+    grids = os.listdir(grid_dir)
+    scenario = f"Variation_pricing_{penetration}"
+    tariffs = [
+        # 'Er-mv',
+        # 'Er',
+        'Cl',
+        'Clf',
+        'Csl',
+        'Ec',
+        'Edn',
+        'Ec_Clf',
+        # 'Ec_Clf_Sv',
+        # 'Ec_Cl_Sv',
+        'Ec_Sr',
+        'Ec_Sv',
+        'Clf_Sr',
+        # 'Edn_Cl',
+        # 'Edn_Clf'
+    ]
     ts_dir = r"H:\Tariffs\Timeseries"
     solver = "gurobi"
     extract_storage_duration = True
@@ -31,18 +81,30 @@ if __name__ == "__main__":
     # load scenario and remove household load
     scenario_dict_base = base_scenario()
     scenario_dict_base["solver"] = solver
-    scaling_hh = 2*19.2
-    ts_customer_group = pd.read_csv(
-        os.path.join(ts_dir, "HH", "Ec", "timeseries.csv"),
-        index_col=0, parse_dates=True).sum(axis=1).multiply(scaling_hh)
-    scenario_dict_base["ts_demand"]["Residential"] = -ts_customer_group
+
     # calculate storage equivalent for different tariffs
     for tariff in tariffs:
         scenario_dict = scenario_dict_base.copy()
-        ts_customer_group = pd.read_csv(
-            os.path.join(ts_dir, "EV_HP", tariff, "timeseries.csv"),
-            index_col=0, parse_dates=True).sum(axis=1).multiply(scaling_hh)
-        scenario_dict["ts_demand"]["Residential_new"] = ts_customer_group
+        # load timeseries for all grids
+        ts_households_orig = pd.Series(index=scenario_dict["ts_demand"].index, data=0)
+        ts_customer_group = pd.DataFrame()
+        for grid_id in grids:
+            try:
+                int(grid_id)
+            except:
+                continue
+            ts_customer_group_grid, ts_households_orig = load_adapted_timeseries_per_grid(
+                gd_dir=grid_dir,
+                gd_id=grid_id,
+                nt=tariff,
+                ts_hh=ts_households_orig
+            )
+            ts_customer_group = pd.concat([ts_customer_group, ts_customer_group_grid], axis=1)
+        scaling_hh = 19400/len(ts_customer_group.columns)
+        scenario_dict["ts_demand"]["Residential_new"] = ts_customer_group.sum(axis=1).multiply(scaling_hh)
+        scenario_dict_base["ts_demand"]["Residential"] = -ts_households_orig.multiply(scaling_hh)
+        scenario_dict["ts_demand"]["Residential_new"].to_csv(os.path.join(res_dir, f"ts_hh_{tariff}.csv"))
+        ts_households_orig.multiply(scaling_hh).to_csv(os.path.join(res_dir, f"ts_hh_orig.csv"))
         # shift timeseries
         scenario_dict = adjust_timeseries_data(scenario_dict)
         new_res_load = get_new_residual_load(scenario_dict)
